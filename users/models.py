a4b9ff64303a40bca5743b11b688aab44aa78882
Kipronokirui <victorkipronokirui2@gmail.com>
2023-05-15 18:30:27 +0300

    Users

--- users/models.py
+++ users/models.py
@@ -9,3 +9,9 @@ class User(AbstractUser):
 
     def __str__(self):
         return self.username
+
+class Student(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    
+    def __str__(self):
+        return self.user.username
